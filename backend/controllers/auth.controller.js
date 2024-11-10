26227fc31c09b3dba1a17bf95a02324d3d014b76
DussaUday <dussauday469@gmail.com>
2024-11-10 14:47:02 +0530

    Your commit message here

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -22,8 +22,8 @@ export const signup = async (req, res) => {
 
 		// https://avatar-placeholder.iran.liara.run/
 
-		const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
-		const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+		const boyProfilePic = `https://xsgames.co/randomusers/avatar.php?g=male${username}`;
+		const girlProfilePic = `https://xsgames.co/randomusers/avatar.php?g=female${username}`;
 
 		const newUser = new User({
 			fullName,
